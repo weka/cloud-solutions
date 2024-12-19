c33730abc56c4c041b46a1e64817ee48c1e2f7ca
Assaf Giladi <assaf@weka.io>
2024-12-19 09:59:03 +0000

    feat: add prompt for the config options

--- aws/sagemaker-hyperpod/deploy.sh
+++ aws/sagemaker-hyperpod/deploy.sh
@@ -25,8 +25,13 @@ AWS_REGION="${AWS_REGION:-us-west-1}"
 INSTANCE_COUNT="${INSTANCE_COUNT:-1}"
 CONTROLLER_INSTANCE_TYPE="${CONTROLLER_INSTANCE_TYPE:-m5.12xlarge}"
 LOGIN_GROUP_INSTANCE_TYPE="${LOGIN_GROUP_INSTANCE_TYPE:-m5.4xlarge}"
-EBS_VOLUME_SIZE="100"
-LOGIN_GROUP_EBS_VOLUME_SIZE="100"
+LOGIN_GROUP_INSTANCE_COUNT="${LOGIN_GROUP_INSTANCE_COUNT:-1}"
+WORKER_EBS_VOLUME_SIZE="${WORKER_EBS_VOLUME_SIZE:-500}"
+CONTROLLER_EBS_VOLUME_SIZE="${CONTROLLER_EBS_VOLUME_SIZE:-500}"
+LOGIN_GROUP_EBS_VOLUME_SIZE="${LOGIN_GROUP_EBS_VOLUME_SIZE:-500}"
+LOGIN_GROUP_NAME="${LOGIN_GROUP_NAME:-login-group}"
+CONTROLLER_GROUP_NAME="${CONTROLLER_GROUP_NAME:-controller-machine}"
+WORKER_GROUP_NAME="${WORKER_GROUP_NAME:-worker-group-1}"
 
 cd LifecycleScripts
 if [[ "$OSTYPE" == "darwin"* ]]; then
@@ -39,11 +44,11 @@ cat > base-config/provisioning_parameters.json << EOL
 {
   "version": "1.0.0",
   "workload_manager": "slurm",
-  "controller_group": "controller-machine",
-  "login_group": "login-group",
+  "controller_group": "${CONTROLLER_GROUP_NAME}",
+  "login_group": "${LOGIN_GROUP_NAME}",
   "worker_groups": [
     {
-      "instance_group_name": "worker-group-1",
+      "instance_group_name": "${WORKER_GROUP_NAME}",
       "partition_name": "${INSTANCE_TYPE}"
     }
   ],
@@ -57,17 +62,18 @@ mkdir -p base-config/weka
 cp set_weka.sh weka_slurm.py utils.py update_slurm_conf.sh base-config/weka
 aws --region "$AWS_REGION" s3 cp --recursive base-config/ s3://${BUCKET}/src
 
-cat > cluster-config.json << EOL
+cluster_config_file="cluster-config-$(date '+%Y-%m-%d_%H:%M:%S').json"
+cat > "$cluster_config_file" << EOL
 {
     "ClusterName": "${CLUSTER_NAME}",
     "InstanceGroups": [
       {
-        "InstanceGroupName": "controller-machine",
+        "InstanceGroupName": "${CONTROLLER_GROUP_NAME}",
         "InstanceType": "ml.${CONTROLLER_INSTANCE_TYPE}",
         "InstanceStorageConfigs": [
           {
             "EbsVolumeConfig": {
-              "VolumeSizeInGB": ${EBS_VOLUME_SIZE}
+              "VolumeSizeInGB": ${CONTROLLER_EBS_VOLUME_SIZE}
             }
           }
         ],
@@ -80,7 +86,7 @@ cat > cluster-config.json << EOL
         "ThreadsPerCore": 1
       },
       {
-        "InstanceGroupName": "login-group",
+        "InstanceGroupName": "${LOGIN_GROUP_NAME}",
         "InstanceType": "ml.${LOGIN_GROUP_INSTANCE_TYPE}",
         "InstanceStorageConfigs": [
           {
@@ -89,7 +95,7 @@ cat > cluster-config.json << EOL
             }
           }
         ],
-        "InstanceCount": 1,
+        "InstanceCount": ${LOGIN_GROUP_INSTANCE_COUNT},
         "LifeCycleConfig": {
           "SourceS3Uri": "s3://${BUCKET}/src",
           "OnCreate": "on_create.sh"
@@ -98,8 +104,15 @@ cat > cluster-config.json << EOL
         "ThreadsPerCore": 1
       },
       {
-        "InstanceGroupName": "worker-group-1",
+        "InstanceGroupName": "${WORKER_GROUP_NAME}",
         "InstanceType": "ml.${INSTANCE_TYPE}",
+        "InstanceStorageConfigs": [
+          {
+            "EbsVolumeConfig": {
+              "VolumeSizeInGB": ${WORKER_EBS_VOLUME_SIZE}
+            }
+          }
+        ],
         "InstanceCount": ${INSTANCE_COUNT},
         "LifeCycleConfig": {
           "SourceS3Uri": "s3://${BUCKET}/src",
@@ -116,26 +129,29 @@ cat > cluster-config.json << EOL
 }
 EOL
 
-if [[ -n "$TRAINING_PLAN_ARN" ]]; then
-cat > add_train_plan.py << EOL
-import json
-conf = json.load(open('cluster-config.json'))
-for instance_group in conf['InstanceGroups']:
-    if instance_group['InstanceGroupName'] == 'worker-group-1':
-      instance_group['TrainingPlanArn'] = "$TRAINING_PLAN_ARN"
-      break
-json.dump(conf, open('cluster-config.json', 'w'), indent=2)
-EOL
-python3 add_train_plan.py
-rm add_train_plan.py
-fi
-
-
-aws --region "$AWS_REGION" sagemaker create-cluster --cli-input-json "file://cluster-config.json" --output text --no-cli-pager
-rm cluster-config.json base-config/provisioning_parameters.json base-config/lifecycle_script.py
+rm base-config/provisioning_parameters.json base-config/lifecycle_script.py
 rm -rf base-config/weka
 if [[ "$OSTYPE" == "darwin"* ]]; then
   sed -i '' "s/backend_ip=.*/backend_ip='<place holder>'/" set_weka.sh
 else
     sed -i "s/backend_ip=.*/backend_ip='<place holder>'/" set_weka.sh
 fi
+
+if [[ -n "$TRAINING_PLAN_ARN" ]]; then
+cat > add_train_plan.py << EOL
+import json
+conf = json.load(open('$cluster_config_file'))
+for instance_group in conf['InstanceGroups']:
+    if instance_group['InstanceGroupName'] == "${WORKER_GROUP_NAME}":
+      instance_group['TrainingPlanArn'] = "$TRAINING_PLAN_ARN"
+      break
+json.dump(conf, open('$cluster_config_file', 'w'), indent=2)
+print(json.dumps(conf, indent=2))
+EOL
+python3 add_train_plan.py
+rm add_train_plan.py
+fi
+
+echo "cluster config file location: $(pwd)/$cluster_config_file"
+
+aws --region "$AWS_REGION" sagemaker create-cluster --cli-input-json "file://$cluster_config_file" --output text --no-cli-pager
